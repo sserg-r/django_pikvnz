d27df3d5aa8fc065eaea173174aeed729255aa53
sserg-r <rusetskyserg@gmail.com>
2021-12-06 14:18:25 +0300

    add database connections

--- testapp/urls.py
+++ testapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('map', views.MapView.as_view(), name='map'),
     path('list', views.ListView.as_view(), name='list'),
     path('ajax/load-cities/', views.load_subname, name='ajax_load_subnames'),
+    path('tiles/<int:z>/<int:x>/<int:y>.pbf', views.get_tile, name='tileserver'),    
     
 ]
